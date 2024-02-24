edcbb599dc0870b94a77b23c6aa960fc0788fd69
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-23 19:53:19 -0600

    Eliminando el foorer

--- src/app/no-autorizado/page.jsx
+++ src/app/no-autorizado/page.jsx
@@ -17,18 +17,6 @@ export default function Unauthorized() {
           </a>
         </p>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
