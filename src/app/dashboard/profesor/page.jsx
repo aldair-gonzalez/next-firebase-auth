fa385d813640de798d909689b31d190f4739039b
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-23 19:50:48 -0600

    Añadiendo los dashboards

---
+++ src/app/dashboard/profesor/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+const Page = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center flex-col gap-5">
+      <h1>Dashboard de profesor</h1>
+      <button className="text-blue-500 hover:opacity-80" onClick={() => router.back()}>Regresar</button>
+    </div>
+  );
+};
+export default Page;
